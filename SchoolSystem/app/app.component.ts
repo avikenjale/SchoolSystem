f994f246aa26913bd6e4d2f7c1b0fc89c3f7c0c8
avikenjale <avikenjale@gmail.com>
2016-02-10 12:54:47 -0500

    Main Application
    Main Application

---
+++ SchoolSystem/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from 'angular2/core';
+import {StudentComponent} from './student/student.component';
+
+@Component({
+    selector: 'app-component',
+    templateUrl: 'app/app.component.html',
+    directives: [StudentComponent]
+})
+
+export class AppComponent { }
