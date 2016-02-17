9f734ec49eadc526e4764a8af64d80c87cb4ac19
avikenjale <avikenjale@gmail.com>
2016-02-16 23:07:14 -0500

    AvinashHome Branch

--- SchoolSystem/app/student/student.component.ts
+++ SchoolSystem/app/student/student.component.ts
@@ -1,7 +1,26 @@
 ﻿import {Component} from 'angular2/core';
-
+import {Student} from './student';
 @Component({
-template:'<div class="btn btn-default"><h1>Student</h1></div>'
+    templateUrl: '/app/student/student.component.html'
 })
 
-export class StudentComponent{}
+export class StudentComponent {
+    public Students: Student[];
+    public Student: Student;
+
+    public ID: number = 0;
+    public FirstName: string = '';
+    public LastName: string = '';
+    public BirthDate: string = '';
+    public Gender: string = '';
+
+    constructor() {
+        this.Students = [{ ID: 1, FirstName: "Avinash", LastName: "Kenjale", BirthDate: "12-08-1982", Gender: "Male" },
+            { ID: 2, FirstName: "Sampada", LastName: "Kasture", BirthDate: "10-05-1984", Gender: "Female" }];
+
+    }
+
+    public saveStudent() {
+        this.Students.push(new Student(this.ID, this.FirstName, this.LastName, this.BirthDate, this.Gender));
+    }
+}
---
+++ SchoolSystem/app/student/student.ts
@@ -0,0 +1,4 @@
+﻿export class Student {
+    constructor(public ID: number, public FirstName: string, public LastName: string, public BirthDate: string, public Gender: string)
+    { }
+}
